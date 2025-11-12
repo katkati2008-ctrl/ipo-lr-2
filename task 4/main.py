caeabeaa4716b962122e4b64f78e146fa6248109
Katherine <kat.kati2008@gmail.com>
2025-11-12 19:43:43 +0300

    add comment

--- task 4/main.py
+++ task 4/main.py
@@ -1,6 +1,6 @@
-import math
-x=int(input("Введите значение x:"))
-y=int(input("Введите значение y:"))
-z=int(input("Введите значение z:"))
-b= (math.sqrt(10*(math.pow(x,1/3)+math.pow(x,y+2))))*(pow(math.asin(z),2)-abs(x-y))
-print(b)
+import math #подключаем модуль math
+x=int(input("Введите значение x:")) #вводим значение x
+y=int(input("Введите значение y:")) #вводим значение y
+z=int(input("Введите значение z:")) #вводим значенине z
+b= (math.sqrt(10*(math.pow(x,1/3)+math.pow(x,y+2))))*(pow(math.asin(z),2)-abs(x-y)) #присваиваем переменной b математическое выражение
+print(b) #выводим значение b
